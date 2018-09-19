1cea7047c0d2292900f62c9e04de267acf3803f9
joshkendrick <joshkendrick@users.noreply.github.com>
2018-09-18 21:32:19 -0500

    reading stdin
    taken from golang docs
    
    initial commit with plans for commands to support

---
+++ README.md
@@ -0,0 +1,21 @@
+Run the program: ./tags-manager.exe
+
+###TODO:
+
+Add directories to index tags
+`index <file-or-dir>`
+
+List all tags
+`list`
+
+List all files associated with a tag
+`list <tag>`
+
+Edit a tag to another tag
+`edit <old-tag> <new-tag>`
+
+Add a tag to all files with another tag
+`add <tag1> <tag2>`
+
+Remove a tag from all files with that tag
+`remove <tag>`
---
+++ main.go
@@ -0,0 +1,21 @@
+// Author: Josh Kendrick
+// Version: v0.1.0
+// Do whatever you want with this code
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
