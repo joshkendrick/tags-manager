e12080c2dee66237b1e5c24edfc24c3e2df8f6a1
joshkendrick <joshkendrick@users.noreply.github.com>
2018-09-18 22:12:23 -0500

    pattern matching, initial logic branching

--- main.go
+++ main.go
@@ -8,13 +8,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func main() {
+	argsRegEx := regexp.MustCompile("'?[[:graph:]]+'?")
+
+	// read from stdin
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		line := scanner.Text()
+
+		// split the line into arguments
+		args := argsRegEx.FindAllString(line, -1)
+
+		// perform action based on the command (first arg)
+		cmd := args[0]
+		switch cmd {
+		case "exit":
+			fmt.Println("GOODBYE")
+			return
+		case "index":
+			// TODO
+		case "add":
+			// TODO
+			// TODO
+		}
 	}
+
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
